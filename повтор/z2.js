d67056ab883c255111ef5d27077310f536c6414f
Zalupa33X <93934468+gibronic@users.noreply.github.com>
2022-06-20 17:51:03 +0300

    Add files via upload

---
+++ повтор/z2.js
@@ -0,0 +1,9 @@
+/*let name = `Петро`;
+let age = 15;
+console.log(` Я${name}! Мені ${age} років`);*/
+
+
+let lp="Петро";
+let lp=9;
+let lp="7 квітня"
+console.log(` 
---
+++ повтор/z4.js
@@ -0,0 +1,8 @@
+let x="Петро";
+let y="Airpods2";
+let tovar_name=4000;
+let fg=1;
+let chislo=350;
+let dostavka=60;
+let zag=tovar_name+chislo+dostavka;
+console.log(`Вітаю, ${x}!. Ваше замовлення ${y} за ціною- ${tovar_name}грн. Гарантія на ${fg} рік за вартістю- ${chislo}грн Вартість доставки- ${60}грн Прийнято та буде оброблення 24 години. Загальна вартість замовлення - ${zag}`)
