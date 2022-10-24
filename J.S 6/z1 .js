42d88bbfd97a9ddb91c1b3000fd886dc99ad50fd
Zalupa33X <93934468+gibronic@users.noreply.github.com>
2022-10-24 17:50:13 +0300

    Add files via upload

---
+++ J.S 6/z1 .js
@@ -0,0 +1,5 @@
+﻿let imya = "Владислав";
+let predmet = "Українська мова"
+let otchinky = [];
+for(i=0;i<10;)
+console.log(`Вітаю ${imya}! Кількість оцінок з предмету ${predmet.length} рівна 9. Остання оцінка -12`)
---
+++ J.S 6/zzzzzzzzzz2.js
@@ -0,0 +1,19 @@
+﻿
+
+let massive = [];
+
+
+for(i=0;i<5;i++){
+	massive[i] = prompt(`Введи ім'я`)
+console.log(`Петя номер ${i+1}`)
+}
+
+
+
+
+
+
+
+
+
+
